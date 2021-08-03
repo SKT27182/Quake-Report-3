7923f86a20cc47d80accbaac1a53ea38dc276563
Shailja Kant Tiwari <smartshailjakant1998@gmail.com>
2021-08-04 00:23:58 +0530

    Created a Eartquakes DataType class

---
+++ app/src/main/java/com/example/android/quakereport/Earthquakes.java
@@ -0,0 +1,20 @@
+package com.example.android.quakereport;
+
+public class Earthquakes {
+
+    private String mMagnitude;
+    private String mPlace;
+    private String mTime;
+
+    public Earthquakes(String magnitude, String place, String time){
+        mMagnitude = magnitude;
+        mPlace = place;
+        mTime = time;
+    }
+
+    public String getmPlace(){return mPlace;}
+
+    public String getmMagnitude(){return mMagnitude;}
+
+    public String getmTime(){return mTime;}
+}
